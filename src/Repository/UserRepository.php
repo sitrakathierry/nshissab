5d9e540ab49988f0cdc4d0f88a206c6c5a11e3c0
sitrakathierry <sitrakathierryfr@gmail.com>
2023-04-13 12:08:51 +0300

    sql

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -58,10 +58,10 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 
     public function findManager($agence)
     {
-        $sql = "SELECT * FROM `user` WHERE `agence_id` = ? AND FIND_IN_SET(?, roles) > 0" ;
+        $sql = "SELECT * FROM `user` WHERE `agence_id` = ? ORDER BY id ASC LIMIT 1" ;
         $conn = $this->getEntityManager()->getConnection();
         $stmt = $conn->prepare($sql);
-        $resultSet = $stmt->executeQuery([$agence,'MANAGER']);
+        $resultSet = $stmt->executeQuery([$agence]);
         return $resultSet->fetchAssociative();
     }
 
