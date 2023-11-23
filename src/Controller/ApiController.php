9fb7f46243babb4b51ef34e61edc95cbaa0ef359
sitrakathierry <sitrakathierryfr@gmail.com>
2023-11-23 05:24:09 +0300

    api insert

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,9 +20,9 @@ class ApiController extends AbstractController
         $dbname = "bazarbdd"; // le nom de votre base de données
 
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn = new \PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             // Définir le mode d'erreur de PDO sur Exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // Préparer la requête d'insertion
             $stmt = $conn->prepare("INSERT INTO compte (nom, prenom) VALUES (:valeur1, :valeur2)");
@@ -38,7 +39,7 @@ class ApiController extends AbstractController
 
             echo "Données insérées avec succès";
 
-        } catch(PDOException $e) {
+        } catch(\PDOException $e) {
             echo "La connexion a échoué : " . $e->getMessage();
         }
     }
