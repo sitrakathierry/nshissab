5794506842b453f33c3d198501a965c901d40774
sitrakathierry <sitrakathierryfr@gmail.com>
2023-12-08 14:39:28 +0300

    mise à jour

--- src/Service/AppService.php
+++ src/Service/AppService.php
@@ -451,7 +451,7 @@ class AppService extends AbstractController
         $client = new Client($basic);
 
         $response = $client->sms()->send(
-            new SMS("261327609366", "HIKAM", 'Bonjour Cher ami, bienvenue sur shissab (Hahah)')
+            new SMS("261345481995", "HIKAM", 'Bonjour Cher ami, bienvenue sur bazardagoni')
         );
         
         $message = $response->current();
